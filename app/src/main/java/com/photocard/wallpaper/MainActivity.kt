f6f5f16c245569dbe76f6b21eab11cac61abca00
배용표 <yp.bae@specupad.com>
2019-08-27 12:59:54 +0900

    TouchImageView 생성 및 Crop Lib 삭제

--- app/src/main/java/com/photocard/wallpaper/MainActivity.kt
+++ app/src/main/java/com/photocard/wallpaper/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.photocard.wallpaper
 
 import android.app.Activity
+import android.app.WallpaperManager
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
-    private val imageUri = "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.eyeem.com%2Fthumb%2Fac369b97a1438d667014f8a329da431b0364c240-1469397546815%2F1280%2F1280&type=b400"
+    private val imageUri =
+        "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.eyeem.com%2Fthumb%2Fac369b97a1438d667014f8a329da431b0364c240-1469397546815%2F1280%2F1280&type=b400"
     private val typeToImage = "image/*"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*button1.setOnClickListener {
+
+            val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER).apply {
+                putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, ComponentName(this@MainActivity, WallpaperTestService::class.java))
+//                component = ComponentName(this@MainActivity, WallpaperTestService::class.java)
+            }
+            startActivity(intent)
+
+        }*/
+
+
 
     }
 }
