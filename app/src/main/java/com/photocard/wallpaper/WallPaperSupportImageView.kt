0dc26f04ac188709bf9e882895c5bf2f71d1404b
배용표 <yp.bae@specupad.com>
2019-08-30 11:16:12 +0900

    원하는 영역 지정 배경 설정.

--- app/src/main/java/com/photocard/wallpaper/WallPaperSupportImageView.kt
+++ app/src/main/java/com/photocard/wallpaper/WallPaperSupportImageView.kt
@@ -6,10 +6,7 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import androidx.annotation.RequiresPermission
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.util.jar.Manifest
 import kotlin.math.abs
 
@@ -34,48 +31,36 @@ class WallPaperSupportImageView @JvmOverloads constructor(
                 Bitmap.Config.ARGB_8888
             )
 
-
             val canvas = Canvas(bitmap)
-
 
             drawable.run {
                 setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
                 draw(canvas)
             }
 
-            val resizeBitmap =
+            val resizeBitmap = async {
                 Bitmap.createScaledBitmap(
                     bitmap,
                     getImageWidth().toInt(),
                     getImageHeight().toInt(),
                     true
                 )
+            }
 
+            try {
+                val cropBitmap = Bitmap.createBitmap(
+                    resizeBitmap.await(),
+                    viewLeftTrans + deviceForegroundBoxSize.left.toInt(),
+                    viewTopTrans + deviceForegroundBoxSize.top.toInt(),
+                    deviceForegroundBoxSize.right.toInt(),
+                    deviceForegroundBoxSize.bottom.toInt()
+                )
 
-            Log.i(
-                "BitmapSizePrint", """
-            |
-            |Width = ${drawable.intrinsicWidth},
-            |Height = ${drawable.intrinsicHeight}
-            |reSizeBitmap Width = ${getImageWidth().toInt()},
-            |reSizeBitmap Height = ${getImageHeight().toInt()}
-            |LEFT = ${viewLeftTrans + deviceForegroundBoxSize.left.toInt()},
-            |TOP = ${viewTopTrans + deviceForegroundBoxSize.top.toInt()},
-            |RIGHT = ${viewLeftTrans + deviceForegroundBoxSize.right.toInt()},
-            |BOTTOM = ${viewTopTrans + deviceForegroundBoxSize.bottom.toInt()}
-            |viewLeftTrans = $viewLeftTrans
-            |viewTopTrans = $viewTopTrans"""
-            )
-            val cropBitmap = Bitmap.createBitmap(
-                resizeBitmap,
-                viewLeftTrans + deviceForegroundBoxSize.left.toInt(),
-                viewTopTrans + deviceForegroundBoxSize.top.toInt(),
-                viewLeftTrans + deviceForegroundBoxSize.right.toInt(),
-                viewTopTrans + deviceForegroundBoxSize.bottom.toInt()
-            )
-
-            withContext(Dispatchers.Main) {
-                WallpaperManager.getInstance(context).setBitmap(cropBitmap)
+                withContext(Dispatchers.Main) {
+                    WallpaperManager.getInstance(context).setBitmap(cropBitmap)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
 
         }
