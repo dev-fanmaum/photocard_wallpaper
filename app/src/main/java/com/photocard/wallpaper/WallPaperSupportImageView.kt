35bf4b832aa7e8eb8f3ef58961f698ff643cf10f
배용표 <yp.bae@specupad.com>
2019-09-02 17:07:27 +0900

    WallPaperSupportImageView Custom Attribute 추가.

--- app/src/main/java/com/photocard/wallpaper/WallPaperSupportImageView.kt
+++ app/src/main/java/com/photocard/wallpaper/WallPaperSupportImageView.kt
@@ -2,12 +2,14 @@ package com.photocard.wallpaper
 
 import android.app.WallpaperManager
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Matrix
-import android.graphics.Paint
+import android.graphics.*
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import androidx.annotation.IntRange
 import androidx.annotation.RequiresPermission
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlin.math.abs
@@ -22,6 +24,54 @@ class WallPaperSupportImageView @JvmOverloads constructor(
     interface WallPaperCallBack {
         fun error(e: Throwable)
         fun complete()
+    }
+
+    private val overlayDrawable: Drawable?
+
+    /**
+    100% — FF
+    95% — F2
+    90% — E6
+    85% — D9
+    80% — CC
+    75% — BF
+    70% — B3
+    65% — A6
+    60% — 99
+    55% — 8C
+    50% — 80
+    45% — 73
+    40% — 66
+    35% — 59
+    30% — 4D
+    25% — 40
+    20% — 33
+    15% — 26
+    10% — 1A
+    5%  — 0D
+    0% —  00
+     */
+    @IntRange(from = 0, to = 255)
+    private val overlayDrawableAlpha: Int
+
+    init {
+        if (attributeSet != null) {
+            val typeAttr =
+                context.obtainStyledAttributes(attributeSet, R.styleable.WallPaperSupportImageView)
+
+            overlayDrawable =
+                typeAttr.getDrawable(R.styleable.WallPaperSupportImageView_overlay_drawable)
+            overlayDrawable?.invalidateSelf()
+            overlayDrawableAlpha =
+                typeAttr.getInteger(
+                    R.styleable.WallPaperSupportImageView_overlay_drawable_alpha,
+                    255
+                )
+            typeAttr.recycle()
+        } else {
+            overlayDrawable = null
+            overlayDrawableAlpha = 255
+        }
     }
 
     @Volatile
@@ -100,12 +150,35 @@ class WallPaperSupportImageView @JvmOverloads constructor(
 
     override fun onDrawForeground(canvas: Canvas) {
         super.onDrawForeground(canvas)
-        val paint = Paint().apply {
-            strokeWidth = 4f
-            style = Paint.Style.STROKE
-        }
 
-        canvas.drawRect(deviceForegroundBoxSize, paint)
+        if (overlayDrawable == null) {
+            val paint = Paint().apply {
+                strokeWidth = 4f
+                style = Paint.Style.STROKE
+            }
+
+            canvas.drawRect(deviceForegroundBoxSize, paint)
+        } else {
+            overlayDrawable.setBounds(
+                0,
+                0,
+                deviceSizeFromOverlayToWidthSize,
+                deviceSizeFromOverlayToHeightSize
+            )
+            val overlayBitmap = Bitmap.createBitmap(
+                deviceSizeFromOverlayToWidthSize,
+                deviceSizeFromOverlayToHeightSize,
+                Bitmap.Config.ARGB_8888
+            )
+            overlayDrawable.draw(Canvas(overlayBitmap))
+
+            canvas.drawBitmap(
+                overlayBitmap,
+                deviceForegroundBoxSize.left,
+                deviceForegroundBoxSize.top,
+                Paint().apply { alpha = overlayDrawableAlpha }
+            )
+        }
     }
 
 }
