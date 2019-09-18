0b688b56b494e188b463470942aabad01db40eae
배용표 <yp.bae@specupad.com>
2019-09-18 16:47:46 +0900

    move test code Add

--- app/src/androidTest/java/com/example/testbuildervm/ScreenMoveTest.kt
+++ app/src/androidTest/java/com/example/testbuildervm/ScreenMoveTest.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import android.graphics.Rect
 import android.support.constraint.ConstraintLayout
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.ViewAction
+import android.support.test.espresso.action.ViewActions
+
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.photocard.wallpaper.WallPaperView
 import junit.framework.Assert.fail
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.random.Random
 
 
 @RunWith(AndroidJUnit4::class)
@@ -29,8 +34,8 @@ class ScreenMoveTest {
     val button by lazy { activityRule.activity.findViewById<WallPaperView>(R.id.save_button) }
     val buttonEspresso by lazy { onView(withId(R.id.save_button)) }
 
-    @Test
-    fun initScreen() {
+    @Before
+    fun init() {
         activityRule.launchActivity(Intent())
 
         activityRule.activity.runOnUiThread {
@@ -39,7 +44,10 @@ class ScreenMoveTest {
         }
 
         Thread.sleep(1000)
+    }
 
+    @Test
+    fun initScreen() {
 
         val overlayInsideCheck = imageBoxRectChecker()
 
@@ -53,7 +61,23 @@ class ScreenMoveTest {
     @Test
     fun screenMove() {
 
+        randomMoveTaskList(10).asSequence()
+            .forEach {
+                wallpaperImageViewEspresso.perform(it)
+                Thread.sleep(500)
+                imageBoxRectChecker()
+            }
     }
+
+    private val moveMap = mapOf<Int, ViewAction>(
+        0 to ViewActions.swipeLeft(),
+        1 to ViewActions.swipeRight(),
+        2 to ViewActions.swipeUp(),
+        3 to ViewActions.swipeDown()
+    )
+
+    private fun randomMoveTaskList(i: Int) =
+        List(i) { moveMap.get(Random.nextInt(0, moveMap.size)) }
 
 
     private fun imageBoxRectChecker(): String {
